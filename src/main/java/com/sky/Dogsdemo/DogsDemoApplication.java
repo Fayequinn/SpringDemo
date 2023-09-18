990e3cb088fdfe8a90a5463ecffa489806694080
Fayequinn <fayequinn88@hotmail.com>
2023-09-18 12:20:09 +0100

    spring demo

---
+++ src/main/java/com/sky/Dogsdemo/DogsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sky.Dogsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/Dogsdemo/DogsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.Dogsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
